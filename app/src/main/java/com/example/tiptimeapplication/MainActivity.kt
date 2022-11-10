6ede4daeb28bdd02c4bd6bd47d0d28992f9b3230
ayeshaMalikAndroid <ayeshamalik101020@gmail.com>
2022-11-10 16:31:08 +0500

    Calculate Tip

---
+++ app/src/main/java/com/example/tiptimeapplication/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.tiptimeapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.example.tiptimeapplication.databinding.ActivityMainBinding
+import java.text.NumberFormat
+import kotlin.math.floor
+private const val TAG ="MainActivity"
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener { calculateTip() }
+    }
+
+    private fun calculateTip() {
+        val stringInTextField = binding.costOfService.text.toString()
+        val cost = stringInTextField.toDoubleOrNull()
+        if (cost == null) {
+            binding.tipResult.text = ""
+            return
+        }
+
+        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+
+        var tip = tipPercentage * cost
+        if (binding.roundUpSwitch.isChecked) {
+            tip = kotlin.math.ceil(tip)
+            Log.d(TAG, "calculateTip: Ceil value $tip")
+         val t =  floor(tip)
+            Log.d(TAG, "calculateTip: Floor value $t")
+        }
+
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+}
